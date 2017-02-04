b6c56c21f24906a4d92e984577ac7e4bb7da6f3b
fizikBoy <Iskandar684@gmail.com>
2017-02-04 07:08:54 +0000

    Share project "holiday.calculator.branding" into "http://subversion.assembla.com/svn/myspaseforproject"

---
+++ holiday.calculator.branding/trunk/src/main/java/ru/iskandar/holiday/calculator/branding/MainPerspective.java
@@ -0,0 +1,18 @@
+package ru.iskandar.holiday.calculator.branding;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+/** Главная перспектива */
+public class MainPerspective implements IPerspectiveFactory {
+
+	/** Идентификатор перспективы */
+	public static final String ID = "ru.iskandar.holiday.calculator.product.MainPerspective";
+
+	/** {@inheritDoc} */
+	@Override
+	public void createInitialLayout(IPageLayout aLayout) {
+
+	}
+
+}

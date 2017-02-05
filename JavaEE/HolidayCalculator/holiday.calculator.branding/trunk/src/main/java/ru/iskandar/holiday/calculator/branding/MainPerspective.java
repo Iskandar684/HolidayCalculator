81ba3251ea59d552a033c43305f72f7351a73fd2
FizikBoy <FizikBoy@d707e1d9-27c1-4415-853f-ed5ac431f276>
2017-02-05 16:53:13 +0000

    Добавлено открытие приложения на полный экран

--- JavaEE/HolidayCalculator/holiday.calculator.branding/trunk/src/main/java/ru/iskandar/holiday/calculator/branding/MainPerspective.java
+++ JavaEE/HolidayCalculator/holiday.calculator.branding/trunk/src/main/java/ru/iskandar/holiday/calculator/branding/MainPerspective.java
@@ -1,8 +1,10 @@
 package ru.iskandar.holiday.calculator.branding;
 
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import org.eclipse.ui.IViewLayout;
+import org.eclipse.ui.PlatformUI;
 
 import ru.iskandar.holiday.calculator.ui.NavigationViewPart;
 
@@ -15,17 +17,22 @@ public class MainPerspective implements IPerspectiveFactory {
 	/** {@inheritDoc} */
 	@Override
 	public void createInitialLayout(IPageLayout aLayout) {
+		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		shell.setMaximized(true);
 		aLayout.setEditorAreaVisible(true);
 		aLayout.addPerspectiveShortcut(ID);
+		aLayout.setFixed(true);
+
 		final String contentsID = NavigationViewPart.ID;
 		final float r = 0.27f;
-		aLayout.addView(contentsID, IPageLayout.LEFT, r, aLayout.getEditorArea());
+		aLayout.addStandaloneView(contentsID, true, IPageLayout.LEFT, r, aLayout.getEditorArea());
 		final IViewLayout contentsLayout = aLayout.getViewLayout(contentsID);
 		if (contentsLayout != null) {
 			contentsLayout.setCloseable(false);
 			contentsLayout.setMoveable(false);
 		}
 		aLayout.addShowViewShortcut(contentsID);
+
 	}
 
 }

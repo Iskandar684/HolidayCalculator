31222d89af61ec74a062953a8d734edd47a6e0eb
fizikBoy <Iskandar684@gmail.com>
2017-02-05 11:08:24 +0000

    Добавлена перспектива, панель навигации

--- holiday.calculator.branding/trunk/src/main/java/ru/iskandar/holiday/calculator/branding/MainPerspective.java
+++ holiday.calculator.branding/trunk/src/main/java/ru/iskandar/holiday/calculator/branding/MainPerspective.java
@@ -2,17 +2,28 @@ package ru.iskandar.holiday.calculator.branding;
 
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IViewLayout;
 
 /** Главная перспектива */
 public class MainPerspective implements IPerspectiveFactory {
 
 	/** Идентификатор перспективы */
-	public static final String ID = "ru.iskandar.holiday.calculator.product.MainPerspective";
+	public static final String ID = "ru.iskandar.holiday.calculator.branding.MainPerspective";
 
 	/** {@inheritDoc} */
 	@Override
 	public void createInitialLayout(IPageLayout aLayout) {
-
+		aLayout.setEditorAreaVisible(true);
+		aLayout.addPerspectiveShortcut(ID);
+		final String contentsID = "ru.iskandar.holiday.calculator.ui.NavigationViewPart";
+		final float r = 0.27f;
+		aLayout.addView(contentsID, IPageLayout.LEFT, r, aLayout.getEditorArea());
+		final IViewLayout contentsLayout = aLayout.getViewLayout(contentsID);
+		if (contentsLayout != null) {
+			contentsLayout.setCloseable(false);
+			contentsLayout.setMoveable(false);
+		}
+		aLayout.addShowViewShortcut(contentsID);
 	}
 
 }

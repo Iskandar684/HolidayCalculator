477bb7115c4e67105c86f5ff2ef7b5b36ee862e4
FizikBoy <FizikBoy@d707e1d9-27c1-4415-853f-ed5ac431f276>
2017-03-02 17:14:21 +0000

    Исправлено получение логина текущего пользователя

--- JavaEE/HolidayCalculator/holiday.calculator.service/trunk/src/main/java/ru/iskandar/holiday/calculator/service/ejb/HolidayCalculatorBean.java
+++ JavaEE/HolidayCalculator/holiday.calculator.service/trunk/src/main/java/ru/iskandar/holiday/calculator/service/ejb/HolidayCalculatorBean.java
@@ -1,17 +1,12 @@
 package ru.iskandar.holiday.calculator.service.ejb;
 
-import javax.annotation.Resource;
-import javax.annotation.security.PermitAll;
+import javax.ejb.EJB;
 import javax.ejb.Remote;
-import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import ru.iskandar.holiday.calculator.service.model.HolidayCalculatorModel;
-import ru.iskandar.holiday.calculator.service.model.HolidayCalculatorModelException;
 import ru.iskandar.holiday.calculator.service.model.HolidayCalculatorModelFactory;
+import ru.iskandar.holiday.calculator.service.model.HolidayCalculatorModelLoadException;
 
 /**
  * Сервис учета отгулов
@@ -19,25 +14,18 @@ import ru.iskandar.holiday.calculator.service.model.HolidayCalculatorModelFactor
 @Stateless
 @Remote(IHolidayCalculatorRemote.class)
 public class HolidayCalculatorBean implements IHolidayCalculatorRemote {
-	/***/
-	@Resource
-	private SessionContext _context;
+
+	/** Сервис работы с текущим пользователем */
+	@EJB
+	private ICurrentUserServiceLocal _currentUserServiceLocal;
 
 	/**
 	 * {@inheritDoc}
 	 */
-	@PermitAll
+
 	@Override
-	public HolidayCalculatorModel loadHolidayCalculatorModel() throws HolidayCalculatorModelException {
-		System.out.println(_context.getCallerPrincipal().getClass());
-		System.out.println("callerPrincipal " + _context.getCallerPrincipal());
-		try {
-			System.out.println("fromProp " + new InitialContext().getEnvironment().get(Context.SECURITY_PRINCIPAL));
-		} catch (NamingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return new HolidayCalculatorModelFactory().create();
+	public HolidayCalculatorModel loadHolidayCalculatorModel() throws HolidayCalculatorModelLoadException {
+		return new HolidayCalculatorModelFactory(_currentUserServiceLocal).create();
 	}
 
 }

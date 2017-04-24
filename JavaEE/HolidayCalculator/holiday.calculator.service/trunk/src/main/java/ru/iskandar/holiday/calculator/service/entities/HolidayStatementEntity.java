7857ad40789df341341ae141439bf652c5cba3aa
FizikBoy <FizikBoy@d707e1d9-27c1-4415-853f-ed5ac431f276>
2017-04-24 16:30:25 +0000

    Переопределен equals и hashcode

--- JavaEE/HolidayCalculator/holiday.calculator.service/trunk/src/main/java/ru/iskandar/holiday/calculator/service/entities/HolidayStatementEntity.java
+++ JavaEE/HolidayCalculator/holiday.calculator.service/trunk/src/main/java/ru/iskandar/holiday/calculator/service/entities/HolidayStatementEntity.java
@@ -174,4 +174,35 @@ public class HolidayStatementEntity implements Serializable {
 		_days = aDays;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = (prime * result) + ((_uuid == null) ? 0 : _uuid.hashCode());
+		return result;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HolidayStatementEntity other = (HolidayStatementEntity) obj;
+		if (_uuid == null) {
+			if (other._uuid != null)
+				return false;
+		} else if (!_uuid.equals(other._uuid))
+			return false;
+		return true;
+	}
+
 }
--- JavaEE/HolidayCalculator/holiday.calculator.service/trunk/src/main/java/ru/iskandar/holiday/calculator/service/entities/UserEntity.java
+++ JavaEE/HolidayCalculator/holiday.calculator.service/trunk/src/main/java/ru/iskandar/holiday/calculator/service/entities/UserEntity.java
@@ -144,4 +144,35 @@ public class UserEntity implements Serializable {
 		_employmentDate = aEmploymentDate;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = (prime * result) + ((_uuid == null) ? 0 : _uuid.hashCode());
+		return result;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserEntity other = (UserEntity) obj;
+		if (_uuid == null) {
+			if (other._uuid != null)
+				return false;
+		} else if (!_uuid.equals(other._uuid))
+			return false;
+		return true;
+	}
+
 }

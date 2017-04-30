b03f0a9519ae58abf7b60a57fbc14c31985da2d6
FizikBoy <FizikBoy@d707e1d9-27c1-4415-853f-ed5ac431f276>
2017-04-30 07:22:45 +0000

    Для пользователей, не обладающими полномочием создавать пользователей, скрыт меню верхнего уровня "Пользователи"

--- JavaEE/HolidayCalculator/holiday.calculator.ui/trunk/src/main/java/ru/iskandar/holiday/calculator/ui/menu/UsersRootMenuPM.java
+++ JavaEE/HolidayCalculator/holiday.calculator.ui/trunk/src/main/java/ru/iskandar/holiday/calculator/ui/menu/UsersRootMenuPM.java
@@ -2,7 +2,10 @@ package ru.iskandar.holiday.calculator.ui.menu;
 
 import java.util.Objects;
 
+import org.eclipse.jface.action.ActionContributionItem;
+import org.eclipse.jface.action.ContributionItem;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IContributionManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.swt.widgets.Display;
 
@@ -10,7 +13,9 @@ import ru.iskandar.holiday.calculator.service.model.HolidayCalculatorEvent;
 import ru.iskandar.holiday.calculator.service.model.IHolidayCalculatorModelListener;
 import ru.iskandar.holiday.calculator.ui.HolidayCalculatorModelProvider;
 import ru.iskandar.holiday.calculator.ui.ILoadingProvider.ILoadListener;
+import ru.iskandar.holiday.calculator.ui.ILoadingProvider.LoadStatus;
 import ru.iskandar.holiday.calculator.ui.Messages;
+import ru.iskandar.holiday.calculator.ui.menu.handlers.CreateUserAction;
 
 /**
  * Контроллер меню "Заявления"
@@ -19,14 +24,21 @@ public class UsersRootMenuPM {
 
 	private final MenuManager _menuManager;
 
+	private final ContributionItem _createUserMenuItem;
+
+	private final HolidayCalculatorModelProvider _modelProvider;
+
 	UsersRootMenuPM(MenuManager aMenuManager, final HolidayCalculatorModelProvider aHolidayCalculatorModelProvider) {
 		Objects.requireNonNull(aMenuManager);
 		Objects.requireNonNull(aHolidayCalculatorModelProvider);
 		_menuManager = aMenuManager;
-		update();
 		final HolidayCalculatorModelListener modelListener = new HolidayCalculatorModelListener();
 		aHolidayCalculatorModelProvider.addListener(modelListener);
 		aHolidayCalculatorModelProvider.addLoadListener(new LoadListener());
+		_createUserMenuItem = new ActionContributionItem(new CreateUserAction(aHolidayCalculatorModelProvider));
+		aMenuManager.add(_createUserMenuItem);
+		_modelProvider = aHolidayCalculatorModelProvider;
+		update();
 	}
 
 	private class LoadListener implements ILoadListener {
@@ -80,6 +92,17 @@ public class UsersRootMenuPM {
 	private void update() {
 		_menuManager.setMenuText(getText());
 		_menuManager.update(IAction.TEXT);
+		boolean visible = false;
+		if (LoadStatus.LOADED.equals(_modelProvider.getLoadStatus())) {
+			visible = _modelProvider.getModel().canCreateUser();
+		}
+		_createUserMenuItem.setVisible(visible);
+		_menuManager.setVisible(visible);
+		_menuManager.update(true);
+		IContributionManager parent = _menuManager.getParent();
+		if (parent != null) {
+			parent.update(true);
+		}
 	}
 
 	private String getText() {

ec82ee9789ce731e28cef72e68838230a79227fe
FizikBoy <Iskandar684@gmail.com>
2017-06-17 15:12:02 +0000

    Добавлена фоновая генерация печатной формы заявления на отгул

--- holiday.calculator.ui/trunk/src/main/java/ru/iskandar/holiday/calculator/ui/takeholiday/HTMLDocumentViewer.java
+++ holiday.calculator.ui/trunk/src/main/java/ru/iskandar/holiday/calculator/ui/takeholiday/HTMLDocumentViewer.java
@@ -7,12 +7,17 @@ import java.util.Objects;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
+import ru.iskandar.holiday.calculator.ui.ILoadingProvider.ILoadListener;
+import ru.iskandar.holiday.calculator.ui.ILoadingProvider.LoadStatus;
 import ru.iskandar.holiday.calculator.ui.document.HTMLContent;
 import ru.iskandar.holiday.calculator.ui.document.IHTMLContentProvider;
 
@@ -29,10 +34,47 @@ public class HTMLDocumentViewer {
 	public HTMLDocumentViewer(IHTMLContentProvider aContentProvider) {
 		Objects.requireNonNull(aContentProvider);
 		_contentProvider = aContentProvider;
+
+	}
+
+	private class LoadListener implements ILoadListener {
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void loadStatusChanged() {
+			Display.getDefault().asyncExec(new Runnable() {
+
+				/**
+				 * {@inheritDoc}
+				 */
+				@Override
+				public void run() {
+					refresh();
+				}
+			});
+		}
 	}
 
 	/** Браузер */
 	private Browser _browser;
+
+	private void initListeners() {
+		final LoadListener loadListener = new LoadListener();
+		_contentProvider.addLoadListener(loadListener);
+		_browser.addDisposeListener(new DisposeListener() {
+
+			/**
+			 * {@inheritDoc}
+			 */
+			@Override
+			public void widgetDisposed(DisposeEvent aE) {
+				_contentProvider.removeLoadListener(loadListener);
+
+			}
+		});
+	}
 
 	public Control create(Composite aParent, FormToolkit aToolkit) {
 
@@ -47,15 +89,18 @@ public class HTMLDocumentViewer {
 		_browser = new Browser(main, SWT.NONE);
 		GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true);
 		_browser.setLayoutData(gd);
-
+		initListeners();
 		refresh();
 		return main;
 	}
 
 	public void refresh() {
-		HTMLContent content = _contentProvider.getContent();
-		String text = new String(content.getContent());
-		_browser.setText(text);
+		LoadStatus status = _contentProvider.getLoadStatus();
+		if (LoadStatus.LOADED.equals(status)) {
+			HTMLContent content = _contentProvider.getContent();
+			String text = new String(content.getContent());
+			_browser.setText(text);
+		}
 	}
 
 }

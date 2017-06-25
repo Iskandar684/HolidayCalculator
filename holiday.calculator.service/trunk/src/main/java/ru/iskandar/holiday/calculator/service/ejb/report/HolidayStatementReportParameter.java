d96af90ffc1ece03427c87d09dd2bada58a1c198
FizikBoy <Iskandar684@gmail.com>
2017-06-25 16:08:47 +0000

    В параметр отчета добавлены даты отгула

--- holiday.calculator.service/trunk/src/main/java/ru/iskandar/holiday/calculator/service/ejb/report/HolidayStatementReportParameter.java
+++ holiday.calculator.service/trunk/src/main/java/ru/iskandar/holiday/calculator/service/ejb/report/HolidayStatementReportParameter.java
@@ -1,9 +1,13 @@
 package ru.iskandar.holiday.calculator.service.ejb.report;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.Date;
 import java.util.Objects;
+import java.util.Set;
 
 import ru.iskandar.holiday.calculator.service.model.user.User;
+import ru.iskandar.holiday.calculator.service.utils.DateUtils;
 
 /**
  * Контейнер-параметр для формирования документа "Заявления на отпуск"
@@ -18,12 +22,16 @@ public class HolidayStatementReportParameter implements Serializable {
 	/** Автор */
 	private User _author;
 
+	private Set<Date> _dates;
+
 	/**
 	 *
 	 */
-	HolidayStatementReportParameter(User aAuthor) {
+	HolidayStatementReportParameter(User aAuthor, Set<Date> aDates) {
 		Objects.requireNonNull(aAuthor);
+		Objects.requireNonNull(aDates);
 		_author = aAuthor;
+		_dates = aDates;
 	}
 
 	/**
@@ -31,6 +39,17 @@ public class HolidayStatementReportParameter implements Serializable {
 	 */
 	public User getAuthor() {
 		return _author;
+	}
+
+	/**
+	 * @return the dates
+	 */
+	public Set<Date> getDates() {
+		return Collections.unmodifiableSet(_dates);
+	}
+
+	public String format(Set<Date> aDates) {
+		return DateUtils.toString(aDates);
 	}
 
 }
--- holiday.calculator.service/trunk/src/main/java/ru/iskandar/holiday/calculator/service/ejb/report/HolidayStatementReportParameterFactory.java
+++ holiday.calculator.service/trunk/src/main/java/ru/iskandar/holiday/calculator/service/ejb/report/HolidayStatementReportParameterFactory.java
@@ -1,21 +1,27 @@
 package ru.iskandar.holiday.calculator.service.ejb.report;
 
+import java.util.Date;
+import java.util.Set;
+
 import ru.iskandar.holiday.calculator.service.model.user.User;
 
 /**
  *
  */
 public abstract class HolidayStatementReportParameterFactory {
-	
-	public HolidayStatementReportParameter create (){
-		User author = getAuthor() ;
-		if (author==null){
+
+	public HolidayStatementReportParameter create() {
+		User author = getAuthor();
+		if (author == null) {
 			throw new IllegalStateException("Не указан автор");
 		}
-		HolidayStatementReportParameter  param = new HolidayStatementReportParameter  (author) ;
-		return param ;
+		Set<Date> dates = getDates();
+		HolidayStatementReportParameter param = new HolidayStatementReportParameter(author, dates);
+		return param;
 	}
-	
+
 	protected abstract User getAuthor();
+
+	protected abstract Set<Date> getDates();
 
 }

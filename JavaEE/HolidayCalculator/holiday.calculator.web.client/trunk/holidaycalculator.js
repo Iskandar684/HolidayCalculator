c97c4872019b3629556b69d3accf81a47843a3d8
fizikBoy <fizikBoy@d707e1d9-27c1-4415-853f-ed5ac431f276>
2017-09-30 10:57:20 +0000

    Добавлен веб клиент сервиса учета отгулов

---
+++ JavaEE/HolidayCalculator/holiday.calculator.web.client/trunk/holidaycalculator.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	$('#callBt').click(function() {
+		callWS();
+	});
+
+});
+
+function callWS() {
+	$.getJSON(
+			"http://192.168.196.129:8080/holiday-calculator-web-service/user",
+			function(data) {
+				alert(data.lastname +' '+ data.firstname+' '+data.patronymic);
+			});
+}

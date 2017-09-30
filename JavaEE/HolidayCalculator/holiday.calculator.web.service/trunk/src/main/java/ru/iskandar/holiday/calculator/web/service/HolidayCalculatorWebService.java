09484aeec393c5b927a9b50bf91528487049983a
FizikBoy <FizikBoy@d707e1d9-27c1-4415-853f-ed5ac431f276>
2017-09-30 10:04:53 +0000

    Добавлено возвращение заглушки User

--- JavaEE/HolidayCalculator/holiday.calculator.web.service/trunk/src/main/java/ru/iskandar/holiday/calculator/web/service/HolidayCalculatorWebService.java
+++ JavaEE/HolidayCalculator/holiday.calculator.web.service/trunk/src/main/java/ru/iskandar/holiday/calculator/web/service/HolidayCalculatorWebService.java
@@ -1,18 +1,57 @@
 package ru.iskandar.holiday.calculator.web.service;
 
+import java.util.Date;
+import java.util.UUID;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import ru.iskandar.holiday.calculator.service.model.user.User;
+import ru.iskandar.holiday.calculator.service.model.user.UserFactory;
+
 @Path("/")
 public class HolidayCalculatorWebService {
+
+	private class CurrentUserInfoFactory extends UserFactory {
+
+		@Override
+		protected UUID getUUID() {
+			return UUID.randomUUID();
+		}
+
+		@Override
+		protected String getFirstName() {
+			return "Олег";
+		}
+
+		@Override
+		protected String getLastName() {
+			return "Сидоров";
+		}
+
+		@Override
+		protected String getPatronymic() {
+			return "Михайлович";
+		}
+
+		@Override
+		protected Date getEmploymentDate() {
+			return new Date();
+		}
+
+		@Override
+		protected String getLogin() {
+			return "user1";
+		}
+
+	}
 
 	@GET
 	@Path("/user")
 	@Produces({ "application/json" })
-	public String getUser() {
-		// TODO
-		return "{\"result\":\"" + "Алексей" + "\"}";
+	public User getUser() {
+		return new CurrentUserInfoFactory().create();
 	}
 
 }

ccfd59cf1354373e11b082498c9a7199d2bdfccd
fizikBoy <fizikBoy@d707e1d9-27c1-4415-853f-ed5ac431f276>
2017-10-14 12:47:10 +0000

    Добавлен лабел с ФИО

--- JavaEE/HolidayCalculator/holiday.calculator.web.client/trunk/holidaycalculator.js
+++ JavaEE/HolidayCalculator/holiday.calculator.web.client/trunk/holidaycalculator.js
@@ -1,15 +1,32 @@
+var url = "http://192.168.196.129:8080/holiday-calculator-web-service/";
+
+function showFIODialog() {
+	$.getJSON(url + "user", function(data) {
+		alert(data.lastName + ' ' + data.firstName + ' ' + data.patronymic);
+	});
+}
+
+function updateFIO() {
+	$.getJSON(url + "user", function(data) {
+		var fio = data.lastName + ' ' + data.firstName + ' ' + data.patronymic;
+		updateFIO(fio);
+	});
+}
+
+function updateFIO(aFIO) {
+	document.getElementById('fio').innerHTML = aFIO;
+}
+
 $(document).ready(function() {
 
+//	updateFIO () ;
+	$.getJSON(url + "user", function(data) {
+		var fio = data.lastName + ' ' + data.firstName + ' ' + data.patronymic;
+		updateFIO(fio);
+	});
+
 	$('#callBt').click(function() {
-		callWS();
+		showFIODialog();
 	});
 
 });
-
-function callWS() {
-	$.getJSON(
-			"http://192.168.196.129:8080/holiday-calculator-web-service/user",
-			function(data) {
-				alert(data.lastName +' '+ data.firstName+' '+data.patronymic);
-			});
-}

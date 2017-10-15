ea6ceefbcb367a06974d63ddca43e31391f68610
fizikBoy <fizikBoy@d707e1d9-27c1-4415-853f-ed5ac431f276>
2017-10-15 09:59:43 +0000

    Добавлены логин, пароль при вызове методов веб сервиса

--- JavaEE/HolidayCalculator/holiday.calculator.web.client/trunk/holidaycalculator.js
+++ JavaEE/HolidayCalculator/holiday.calculator.web.client/trunk/holidaycalculator.js
@@ -24,14 +24,14 @@ function updateHolidayCount(aCount) {
 $(document).ready(function() {
 
 	// updateFIO () ;
-	$.getJSON(url + "user", function(data) {
+	$.getJSON(url + "user/user1/password1", function(data) {
 		var fio = data.lastName + ' ' + data.firstName + ' ' + data.patronymic;
 		updateFIO(fio);
 	});
 
 	updateHolidayCount(1);
 
-	$.getJSON(url + "HolidaysQuantity", function(data) {
+	$.getJSON(url + "HolidaysQuantity/user1/password1", function(data) {
 		updateHolidayCount(data);
 	});
 

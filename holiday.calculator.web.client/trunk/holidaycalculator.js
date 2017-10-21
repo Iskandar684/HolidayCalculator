c1630d19ecff609204bfefad87f48f085934bdc9
fizikBoy <Iskandar684@gmail.com>
2017-10-21 09:44:32 +0000

    Рефакторинг

--- holiday.calculator.web.client/trunk/holidaycalculator.js
+++ holiday.calculator.web.client/trunk/holidaycalculator.js
@@ -20,46 +20,37 @@ function updateHolidayCount(aCount) {
 }
 
 function authorization() {
+	$("#dialog_auth").dialog({
+		buttons : {
+			"Вход" : function() {
+				enterPressed();
+				$(this).dialog("close");
+			},
+			"Закрыть" : function() {
+				$(this).dialog("close");
+			}
+		}
+	});
+	$('#user_auth').click(function() {
+		$('#dialog_auth').dialog('open');
+	});
+}
 
-	$(function() {
-		$("#dialog_auth")
-				.dialog(
-						{
-							buttons : {
-								"Вход" : function() {
-									login = document
-											.getElementById('user_name').value;
-									password = document
-											.getElementById('user_password').value;
+function enterPressed() {
+	login = document.getElementById('user_name').value;
+	password = document.getElementById('user_password').value;
 
-									$.getJSON(url + "user/" + login + "/"
-											+ password, function(data) {
-										var fio = data.lastName + ' '
-												+ data.firstName + ' '
-												+ data.patronymic;
-										updateFIO(fio);
-									});
-
-									$.getJSON(url + "HolidaysQuantity/" + login
-											+ "/" + password, function(data) {
-										updateHolidayCount(data);
-									});
-
-									document.getElementById('user_auth').innerHTML = 'Сменить пользователя';
-									document.getElementById("userinfo").style.display = 'block';
-
-									$(this).dialog("close");
-								},
-								"Закрыть" : function() {
-									$(this).dialog("close");
-								}
-							}
-						});
-		$('#user_auth').click(function() {
-			$('#dialog_auth').dialog('open');
-		});
+	$.getJSON(url + "user/" + login + "/" + password, function(data) {
+		var fio = data.lastName + ' ' + data.firstName + ' ' + data.patronymic;
+		updateFIO(fio);
 	});
 
+	$.getJSON(url + "HolidaysQuantity/" + login + "/" + password,
+			function(data) {
+				updateHolidayCount(data);
+			});
+	document.getElementById('user_auth').innerHTML = 'Сменить пользователя';
+	document.getElementById("userinfo").style.display = 'block';
 }
 
 $(document).ready(function() {

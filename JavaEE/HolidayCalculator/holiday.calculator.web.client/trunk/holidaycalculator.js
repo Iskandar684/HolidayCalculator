bd011e8c8557e619e208893809d8e4419e6e2a17
fizikBoy <fizikBoy@d707e1d9-27c1-4415-853f-ed5ac431f276>
2017-10-28 11:48:40 +0000

    Рефакторинг

--- JavaEE/HolidayCalculator/holiday.calculator.web.client/trunk/holidaycalculator.js
+++ JavaEE/HolidayCalculator/holiday.calculator.web.client/trunk/holidaycalculator.js
@@ -1,15 +1,4 @@
 var url = "http://192.168.196.129:8080/holiday-calculator-web-service/";
-var userName;
-var password;
-
-function updateFIO() {
-
-	$.getJSON(url + "user/" + login + "/" + password, function(data) {
-		var fio = data.lastName + ' ' + data.firstName + ' ' + data.patronymic;
-		updateFIO(fio);
-	});
-
-}
 
 function updateFIO(aFIO) {
 	document.getElementById('fio').innerHTML = aFIO;
@@ -38,8 +27,8 @@ function initAuthorizationDialog() {
 }
 
 function login() {
-	userName = document.getElementById('user_name').value;
-	password = document.getElementById('user_password').value;
+	var userName = document.getElementById('user_name').value;
+	var password = document.getElementById('user_password').value;
 	$.getJSON(url + "login/" + userName + "/" + password, function(logged) {
 		if (logged) {
 			loadContentByLoggedUser();
@@ -53,13 +42,12 @@ function login() {
 }
 
 function loadContentByLoggedUser() {
-	$.getJSON(url + "HolidaysQuantity", function(data) {
-		updateHolidayCount(data);
-	});
-
 	$.getJSON(url + "user", function(data) {
 		var fio = data.lastName + ' ' + data.firstName + ' ' + data.patronymic;
 		updateFIO(fio);
+	});
+	$.getJSON(url + "HolidaysQuantity", function(data) {
+		updateHolidayCount(data);
 	});
 }
 

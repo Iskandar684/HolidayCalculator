ec554ea36bb54b1404e5dd5d22dc9d75ee50de90
FizikBoy <Iskandar684@gmail.com>
2019-08-11 09:28:54 +0000

    Добавлено отображение общего количества совпадений и времени поиска

--- holiday.calculator.client-libraries/trunk/src/main/java/ru/iskandar/holiday/calculator/dataconnection/ISearchResult.java
+++ holiday.calculator.client-libraries/trunk/src/main/java/ru/iskandar/holiday/calculator/dataconnection/ISearchResult.java
@@ -4,6 +4,25 @@ import java.util.List;
 
 public interface ISearchResult {
 
+	/**
+	 * Возвращает совпадения.
+	 *
+	 * @return совпадаения.
+	 */
 	List<ISearchHit> getHits();
+
+	/**
+	 * Возвращает время поиска в мс.
+	 *
+	 * @return время поиска
+	 */
+	long getTookInMillis();
+
+	/**
+	 * Возвращает общее количество совпадений.
+	 * 
+	 * @return общее количество совпадений
+	 */
+	long getTotalHitsCount();
 
 }

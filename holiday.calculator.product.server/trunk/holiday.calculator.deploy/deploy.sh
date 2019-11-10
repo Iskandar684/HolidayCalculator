c6bb29bef6deb40ffb77d89d0d8bb70bcdc58962
FizikBoy <Iskandar684@gmail.com>
2019-11-10 17:35:21 +0000

    add filed to deploy

---
+++ holiday.calculator.product.server/trunk/holiday.calculator.deploy/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#загрузка настроек
+source ./deploy.conf;
+source ./functions.conf;
+
+#загрузка файлов
+#download;
+#сборка
+build;
+#удаление предыдущей версии
+#clean;
+#копирование файлов
+#copyFiles;
+#запуск
+#start;
+

4b6c56c7964335ece7373a80d427b07e934b6fc2
fizikboy <Iskandar684@gmail.com>
2019-12-10 18:52:22 +0000

    доработано добавление пользователей

--- holiday.calculator.deploy/addUsers.sql
+++ holiday.calculator.deploy/addUsers.sql
@@ -1,5 +1,5 @@
 --создание пользователей приложения
-INSERT INTO public."ru_iskandar_holiday_calculator_user" (uuid, employmentdate, firstname, lastname, login, note, patronymic)
-VALUES ('23c9edba-41d9-488c-a436-9476689a0316', '2019-12-31 14:19:07.868', 'Админов', 'Админ', 'Админович', 'Учетная запись администратора', 'Лаврентьевич');
+INSERT INTO ru_iskandar_holiday_calculator_user (uuid, employmentdate, firstname, lastname, login, note, patronymic)
+VALUES ('23c9edba-41d9-488c-a436-9476689a0316', '2019-12-31 14:19:07.868', 'Админ', 'Админов', 'admin', 'Учетная запись администратора', 'Админович');
 
 
--- holiday.calculator.deploy/init.sql
+++ holiday.calculator.deploy/init.sql
@@ -5,3 +5,19 @@ CREATE DATABASE holiday_calculator_db;
 -- создание пользователя БД
 CREATE USER holiday_calculator_user WITH password 'qwerty';
 GRANT ALL privileges ON DATABASE holiday_calculator_db TO holiday_calculator_user;
+--создание таблицы пользователей приложения
+CREATE TABLE public.ru_iskandar_holiday_calculator_user (
+  uuid UUID NOT NULL,
+  employmentdate TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  login VARCHAR(255) NOT NULL,
+  note VARCHAR(5000),
+  patronymic VARCHAR(255) NOT NULL,
+  CONSTRAINT ru_iskandar_holiday_calculator_user_pkey PRIMARY KEY(uuid),
+  CONSTRAINT uk_df4onudw808ut4isibfulx4jy UNIQUE(login)
+) 
+WITH (oids = false);
+
+ALTER TABLE public.ru_iskandar_holiday_calculator_user
+  OWNER TO holiday_calculator_user;

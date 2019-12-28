285d2c0338d3501946ff8fc1d6f31c2c9b0b842c
fizikboy <Iskandar684@gmail.com>
2019-12-28 17:20:52 +0000

    Исправлены sql скрипты

--- holiday.calculator.deploy/init.sql
+++ holiday.calculator.deploy/init.sql
@@ -2,8 +2,23 @@
 DROP DATABASE IF EXISTS holiday_calculator_db;
 --создание БД
 CREATE DATABASE holiday_calculator_db;
+\connect holiday_calculator_db;
 -- создание пользователя БД
-CREATE USER holiday_calculator_user WITH password 'qwerty';
+--CREATE USER holiday_calculator_user WITH password 'qwerty';
+
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT                      
+      FROM   pg_catalog.pg_roles
+      WHERE  rolname = 'holiday_calculator_user') THEN
+
+      CREATE ROLE my_user LOGIN PASSWORD 'qwerty';
+   END IF;
+END
+$do$;
+
 GRANT ALL privileges ON DATABASE holiday_calculator_db TO holiday_calculator_user;
 --создание таблицы пользователей приложения
 CREATE TABLE public.ru_iskandar_holiday_calculator_user (

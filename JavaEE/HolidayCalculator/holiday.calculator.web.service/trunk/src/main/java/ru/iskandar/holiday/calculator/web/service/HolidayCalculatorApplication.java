9d070f09a05fcc12111b72ec1ecb2324973c0c88
FizikBoy <FizikBoy@d707e1d9-27c1-4415-853f-ed5ac431f276>
2020-05-17 07:33:25 +0000

    Рефакторинг: -убраны лишние ссылки -добавлен lombok

--- JavaEE/HolidayCalculator/holiday.calculator.web.service/trunk/src/main/java/ru/iskandar/holiday/calculator/web/service/HolidayCalculatorApplication.java
+++ JavaEE/HolidayCalculator/holiday.calculator.web.service/trunk/src/main/java/ru/iskandar/holiday/calculator/web/service/HolidayCalculatorApplication.java
@@ -6,23 +6,26 @@ import java.util.Set;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import org.jboss.logging.Logger;
 
 import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder;
 import io.swagger.v3.oas.integration.OpenApiConfigurationException;
 import io.swagger.v3.oas.integration.SwaggerConfiguration;
-import io.swagger.v3.oas.integration.api.OpenApiContext;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import lombok.extern.jbosslog.JBossLog;
 
+/**
+ * Приложение веб-сервиса учета отгулов.
+ */
 @ApplicationPath("/holiday-calculator-web-service")
+@JBossLog
 public class HolidayCalculatorApplication extends Application {
 
-    private static final Logger LOG = Logger.getLogger(HolidayCalculatorApplication.class);
-
+    /**
+     * Конструктор.
+     */
     public HolidayCalculatorApplication() {
         super();
-        LOG.info("start configure EchoApplication");
         OpenAPI oas = new OpenAPI();
         Info info = new Info().title("Методы веб-сервиса учета отгулов.");
         oas.info(info);
@@ -30,11 +33,11 @@ public class HolidayCalculatorApplication extends Application {
                 new SwaggerConfiguration().openAPI(oas).prettyPrint(true).resourcePackages(
                         Collections.singleton("ru.iskandar.holiday.calculator.web.service"));
         try {
-            OpenApiContext ctx = new JaxrsOpenApiContextBuilder().application(this)
-                    .openApiConfiguration(oasConfig).buildContext(true);
-            LOG.info("OpenApiContext " + ctx);
+            new JaxrsOpenApiContextBuilder<>().application(this).openApiConfiguration(oasConfig)
+                    .buildContext(true);
+            log.info("Swagger успешно настроен.");
         } catch (OpenApiConfigurationException e) {
-            LOG.error("Ошибка настройки swagger.", e);
+            log.error("Ошибка настройки swagger.", e);
             throw new RuntimeException(e.getMessage(), e);
         }
     }
@@ -43,9 +46,7 @@ public class HolidayCalculatorApplication extends Application {
     public Set<Class<?>> getClasses() {
         Set<Class<?>> set = new HashSet<Class<?>>();
         set.add(HolidayCalculatorWebService.class);
-        set.add(HolidayCalculatorApplication.class);
         set.add(io.swagger.v3.jaxrs2.integration.resources.OpenApiResource.class);
-        set.add(io.swagger.v3.oas.integration.GenericOpenApiScanner.class);
         return set;
     }
 }

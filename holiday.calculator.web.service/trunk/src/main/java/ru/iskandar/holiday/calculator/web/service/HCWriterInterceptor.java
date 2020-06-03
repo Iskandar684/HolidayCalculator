a65de6564552547d74bc3b42488f5915b934b94c
FizikBoy <Iskandar684@gmail.com>
2020-06-03 20:58:24 +0000

    Добавлено уточнение сообщения об ошибке.

---
+++ holiday.calculator.web.service/trunk/src/main/java/ru/iskandar/holiday/calculator/web/service/HCWriterInterceptor.java
@@ -0,0 +1,37 @@
+package ru.iskandar.holiday.calculator.web.service;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.ext.WriterInterceptor;
+import javax.ws.rs.ext.WriterInterceptorContext;
+
+import org.jboss.resteasy.core.ResourceInvoker;
+
+import lombok.extern.jbosslog.JBossLog;
+
+@Provider
+@JBossLog
+public class HCWriterInterceptor implements WriterInterceptor {
+
+    @Override
+    public void aroundWriteTo(WriterInterceptorContext ctx)
+            throws IOException, WebApplicationException {
+        log.info("HCWriterInterceptor.aroundWriteTo");
+        Object entity = ctx.getEntity();
+        if (entity instanceof ErrorResponse) {
+            ResourceInvoker invoker = (ResourceInvoker) ctx
+                    .getProperty("org.jboss.resteasy.core.ResourceMethodInvoker");
+            Method m = invoker.getMethod();
+            HCWebMethod webMethodAnnotation = m.getAnnotation(HCWebMethod.class);
+            if (webMethodAnnotation != null) {
+                ((ErrorResponse) entity).setMessage(webMethodAnnotation.errMess());
+                ctx.setEntity(entity);
+            }
+        }
+        ctx.proceed();
+        return;
+    }
+}

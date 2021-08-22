fc1d6a9f2287fbcdae9146bf0136bbb4e971d8d4
FizikBoy <FizikBoy@d707e1d9-27c1-4415-853f-ed5ac431f276>
2021-08-22 12:38:40 +0000

    Реализован deploy клиента.

--- JavaEE/HolidayCalculator/deploy.sh
+++ JavaEE/HolidayCalculator/deploy.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 export SERVER_PATH="/home/iskandar/Desktop/hc-server"
+export SERVER_ARCHIVE_PATH="./holiday.calculator.product.server/trunk/target"
 
-export ARCHIVE_PATH="./holiday.calculator.product.server/trunk/target"
+export CLIENT_PATH="/home/iskandar/Desktop/hc-client"
+export CLIENT_ARCHIVE_PATH="./holiday.calculator.product/trunk/target/products"
 
 
-unzip -o $ARCHIVE_PATH/HolidayCalculator-server-server.zip -d $ARCHIVE_PATH
+function deployServer(){
+   unzip -o $SERVER_ARCHIVE_PATH/HolidayCalculator-server-server.zip -d $SERVER_ARCHIVE_PATH
+   cp -rf $SERVER_ARCHIVE_PATH/HolidayCalculator-server/* $SERVER_PATH
+}
 
 
-cp -rf $ARCHIVE_PATH/HolidayCalculator-server/* $SERVER_PATH
+function deployClient(){
+  rm -rf $CLIENT_PATH
+  unzip -o $CLIENT_ARCHIVE_PATH/holiday.calculator.product-linux.gtk.x86_64.zip -d $CLIENT_PATH
+}
+
+deployServer
+deployClient
